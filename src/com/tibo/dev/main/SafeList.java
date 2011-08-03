5720c1e0adf5b6cea3afd0177de31e184b670bb0
Thibault HUET <thibault.huet@gmail.com>
2011-08-03 20:17:26 +0200

    modification configuration idea

---
+++ src/com/tibo/dev/main/SafeList.java
@@ -0,0 +1,48 @@
+package com.tibo.dev.main;
+
+import java.util.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tibo
+ * Date: 14/07/11
+ * Time: 12:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class SafeList<E> extends ArrayList<E> implements List<E>{
+
+    /**
+     * Get ieme element of the list
+     * @param i index of the element to retrieve
+     * @return the element
+     */
+    @Override
+    public E get(int i) {
+        if (i > this.size()){
+            return null;
+        }
+        return super.get(i);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    /**
+     * Get the first element of the list
+     * @return an Object
+     */
+    public E getFirst(){
+         if (this.size()<1){
+            return null;
+        }
+        return get(0);
+    }
+
+    /**
+     * Get the last element of the list
+     * @return
+     */
+    public E getLast(){
+        if (this.size()<1){
+            return null;
+        }
+        return get(this.size()-1);
+    }
+}
