ba0244f67c95626e3f935a9f02be606d5d01d7ac
Thibault HUET <thibault.huet@gmail.com>
2011-09-19 23:11:15 +0200

    refactoring

--- src/com/tibo/dev/common/EqualsHelper.java
+++ src/com/tibo/dev/common/EqualsHelper.java
@@ -9,18 +9,39 @@ package com.tibo.dev.common;
  */
 public class EqualsHelper {
 
+    /**
+     * Returns the result of equals method on an Object
+     * @param o1
+     * @param o2
+     * @return   true if o1 is the same as o2
+     */
     public static boolean isEquals(Object o1, Object o2) {
 		return (o1 == null) ? (o2 == null) : o1.equals(o2);
 	}
 
+    /**
+     * Returns the hashcode of a given object
+     * @param obj
+     * @return
+     */
     public static int hashCodeOf(Object obj) {
 		return (obj == null) ? 0 : obj.hashCode();
 	}
 
+    /**
+     * Returns the hashcode of a boolean
+     * @param b
+     * @return
+     */
     public static int hashCodeOf(boolean b) {
 		return (b ? 1 : 0);
 	}
 
+    /**
+     * Returns the hascode of a long value
+     * @param value
+     * @return
+     */
     public static int hashCodeOf(long value) {
 		return (int)(value ^ (value >>> 32));
 	}
